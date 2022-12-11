965ef82ac5a9f7a5352321b1b362a256012b5d65
DrFaust <matteo.ielacqua@gmail.com>
2022-12-12 00:45:12 +0100

    converted to cpp style

--- GPU/kernelinclude/md5Cuda.cuh
+++ GPU/kernelinclude/md5Cuda.cuh
@@ -1,71 +1,53 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-#define uint uint32_t
-#define uchar uint8_t
+class MD5
+{
+  public:
+    typedef unsigned int size_type; // must be 32bit
 
-#define block_size 64
+    __device__ __host__ MD5();
+    __device__ __host__ MD5(const uint8_t * text, size_t size);
+    __device__ __host__ void update(const unsigned char *buf, size_type length);
+    __device__ __host__ void update(const char *buf, size_type length);
+    __device__ __host__ MD5 &finalize();
+    __device__ __host__ void exportdigest(uint8_t * dataOut);
 
-#define S11 7
-#define S12 12
-#define S13 17
-#define S14 22
-#define S21 5
-#define S22 9
-#define S23 14
-#define S24 20
-#define S31 4
-#define S32 11
-#define S33 16
-#define S34 23
-#define S41 6
-#define S42 10
-#define S43 15
-#define S44 21
 
-#define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
-#define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
-#define H(x, y, z) ((x) ^ (y) ^ (z))
-#define I(x, y, z) ((y) ^ ((x) | (~z)))
+  private:
+    __device__ __host__ void init();
+    typedef unsigned char uint1; //  8bit
+    typedef unsigned int uint4;  // 32bit
+    enum
+    {
+        blocksize = 64
+    }; // VC6 won't eat a const static int here
 
-#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
+    __device__ __host__ void transform(const uint1 block[blocksize]);
+    __device__ __host__ static void decode(uint4 output[], const uint1 input[], size_type len);
+    __device__ __host__ static void encode(uint1 output[], const uint4 input[], size_type len);
 
-#define FF(a, b, c, d, x, s, ac)                                                                                       \
-    {                                                                                                                  \
-        (a) += F((b), (c), (d)) + (x) + (uint)(ac);                                                                    \
-        (a) = ROTATE_LEFT((a), (s));                                                                                   \
-        (a) += (b);                                                                                                    \
-    }
+    bool finalized;
+    uint1 buffer[blocksize]; // bytes that didn't fit in last 64 byte chunk
+    uint4 count[2];          // 64bit counter for number of bits (lo, hi)
+    uint4 state[4];          // digest so far
+    uint1 digest[16];        // the result
 
-#define GG(a, b, c, d, x, s, ac)                                                                                       \
-    {                                                                                                                  \
-        (a) += G((b), (c), (d)) + (x) + (uint)(ac);                                                                    \
-        (a) = ROTATE_LEFT((a), (s));                                                                                   \
-        (a) += (b);                                                                                                    \
-    }
+    // low level logic operations
+    __device__ __host__ static inline uint4 F(uint4 x, uint4 y, uint4 z);
+    __device__ __host__ static inline uint4 G(uint4 x, uint4 y, uint4 z);
+    __device__ __host__ static inline uint4 H(uint4 x, uint4 y, uint4 z);
+    __device__ __host__ static inline uint4 I(uint4 x, uint4 y, uint4 z);
+    __device__ __host__ static inline uint4 rotate_left(uint4 x, int n);
+    __device__ __host__ static inline void FF(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+    __device__ __host__ static inline void GG(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+    __device__ __host__ static inline void HH(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+    __device__ __host__ static inline void II(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+};
 
-#define HH(a, b, c, d, x, s, ac)                                                                                       \
-    {                                                                                                                  \
-        (a) += H((b), (c), (d)) + (x) + (uint)(ac);                                                                    \
-        (a) = ROTATE_LEFT((a), (s));                                                                                   \
-        (a) += (b);                                                                                                    \
-    }
-
-#define II(a, b, c, d, x, s, ac)                                                                                       \
-    {                                                                                                                  \
-        (a) += I((b), (c), (d)) + (x) + (uint)(ac);                                                                    \
-        (a) = ROTATE_LEFT((a), (s));                                                                                   \
-        (a) += (b);                                                                                                    \
-    }
-
-__device__ __host__ uint byteswap(uint word);
-
-__device__ __host__ void transform(uint state[4], const uchar block[block_size]);
-
-//__device__ __host__ void md5(const uint8_t *data, uint32_t size, uint32_t result[4]);
-
-__host__ void md5_gpu(const uint8_t **data, const uint32_t * sizes, uint32_t** result, uint32_t size, int threads);
-
-__host__ void md5_gpu(const uint8_t * data, uint32_t size, uint32_t result[4]);
+__host__ __device__ void  md5(const uint8_t *data, uint32_t size, uint8_t * result);
+__host__ void md5_gpu(const uint8_t **data, const uint32_t *sizes, uint8_t **result, uint32_t size, int threads);
+__host__ void md5_gpu(const uint8_t *data, uint32_t size, uint8_t * result);
